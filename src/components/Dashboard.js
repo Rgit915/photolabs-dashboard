5ca22ebc558c47dc7a788e3f98d40fa1ae167f60
Rora Alem <rora.goitom@gmail.com>
2023-09-28 21:24:53 -0400

    Update onSelect prop to use arrow function

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -51,10 +51,10 @@ class Dashboard extends Component {
   const panelElements =  (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data).map((panel) => (
     <Panel
       key={panel.id}
-      id={panel.id}
+
       label={panel.label}
       value={panel.value}
-      onSelect={this.selectPanel}
+      onSelect={event => this.selectPanel(panel.id)}
     />
   ));
 
